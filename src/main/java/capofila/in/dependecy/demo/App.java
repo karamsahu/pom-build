023d4c918e12c05c5e26a9e9f728156c8753de90
karamchand sahu <karamchand sahu>
2017-04-20 22:17:59 +0530

    initial commit

---
+++ src/main/java/capofila/in/dependecy/demo/App.java
@@ -0,0 +1,13 @@
+package capofila.in.dependecy.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
